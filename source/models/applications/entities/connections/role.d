b2debbf971527c6b5b934c375f255c4ed75c9443
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-04 21:11:00 +0200

    Update role.d

--- source/models/applications/entities/connections/role.d
+++ source/models/applications/entities/connections/role.d
@@ -4,47 +4,38 @@ module models.applications.connections.role;
 import models.applications;
 
 // Role describing a relationship between a two records.
-  class DAPLConnectionRole : DOOPEntity {
-  this() { super();
-    this.addValues([
-      "connectionRoleId": UUIDAttribute, // Unique identifier of the connection role."]),
-      "importSequenceNumber": NumberAttribute, // Unique identifier of the data import or data migration that created this record."]),
-      "category": StringAttribute, // Categories for connection roles."]),
-      "category_display": StringAttribute, // 
-      "organizationId": UUIDAttribute, // Unique identifier of the organization that this connection role belongs to."]),
-      "solutionId": UUIDAttribute, // Unique identifier of the associated solution."]),
-      "componentState": StringAttribute, // State of the component."]),
-      "componentState_display": StringAttribute, // 
-      "overwriteTime": TimeAttribute, // Date and time when the record was last overwritten."]),
-      "connectionRoleIdUnique": StringAttribute, // Unique identifier of the published or unpublished connection role record."]),
-      "modifiedOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who modified the relationship role."]),
-      "createdOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who created the relationship role."]),
-      "isManaged": BooleanAttribute, // Indicates whether the solution component is part of a managed solution."]),
-      "isCustomizable": BooleanAttribute, // Information that specifies whether this component can be customized."]),
-      "introducedVersiOn": DatetimeAttribute, // Version in which the form is introduced."]),
-    ])
-    .addValues([
-      StateCodeAttribute, // Status of the connection role.
-      StatusCodeAttribute // Reason for the status of the connection role.
-    ]);
+  class DAPLConnectionRoleEntity : DOOPEntity {
+    mixin(EntityThis!("APLConnectionRoleEntity"));
+    
+    override void initialize() {
+      super.initialize;
+      
+      this
+      .addValues([
+        StateCodeAttribute, // Status of the connection role.
+        StatusCodeAttribute // Reason for the status of the connection role.
+      ])
+      .addValues([
+        "connectionRoleId": UUIDAttribute, // Unique identifier of the connection role."]),
+        "importSequenceNumber": NumberAttribute, // Unique identifier of the data import or data migration that created this record."]),
+        "category": StringAttribute, // Categories for connection roles."]),
+        "category_display": StringAttribute, // 
+        "organizationId": UUIDAttribute, // Unique identifier of the organization that this connection role belongs to."]),
+        "solutionId": UUIDAttribute, // Unique identifier of the associated solution."]),
+        "componentState": StringAttribute, // State of the component."]),
+        "componentState_display": StringAttribute, // 
+        "overwriteTime": TimeAttribute, // Date and time when the record was last overwritten."]),
+        "connectionRoleIdUnique": StringAttribute, // Unique identifier of the published or unpublished connection role record."]),
+        "modifiedOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who modified the relationship role."]),
+        "createdOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who created the relationship role."]),
+        "isManaged": BooleanAttribute, // Indicates whether the solution component is part of a managed solution."]),
+        "isCustomizable": BooleanAttribute, // Information that specifies whether this component can be customized."]),
+        "introducedVersiOn": DatetimeAttribute, // Version in which the form is introduced."]),
+      ])
+      .registerPath("applications_connections.roles");
   }
-
-  override string entityClass() { return "aplConnectionRole"; }
-  override string entityClasses() { return "aplConnectionRoles"; }
-
-  this(UUID myId) { 
-    this(); this.id(myId); }
-  this(string myName) { 
-    this(); this.name(myName); }
-  this(UUID myId, string myName) { 
-    this(); this.id(myId).name(myName); }
-  this(Json aJson) { 
-    this(); this.fromJson(aJson); }
-  
-  // mixin(GetEntity!("organization", "organizationId", "APLOrganization"));
 }
-auto APLConnectionRole() { return new DAPLConnectionRole; } 
-auto APLConnectionRole(Json json) { return new DAPLConnectionRole(json); } 
+mixin(EntityCalls!("APLConnectionRoleEntity")); 
 
 version(test_library) {
   unittest {
