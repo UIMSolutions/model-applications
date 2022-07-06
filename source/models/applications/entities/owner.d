1ea87b9b4ea1ffe281bd2e72ec4294315e8b117f
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-07-06 06:29:17 +0200

    Update comments

--- source/models/applications/entities/owner.d
+++ source/models/applications/entities/owner.d
@@ -12,11 +12,11 @@ class DOwnerEntity : DOOPEntity {
 
     this
       .addValues([
-        OwnerIdAttribute, // Unique identifier for the Owner: systemuserid or teamid."]),
+        OwnerIdAttribute, // Unique identifier for the Owner: systemuserid or teamid.
       ])
       .addValues([
         "ownerIdType": StringAttribute, // 
-        "yomiName": StringAttribute, // Pronunciation of the name of the owner, written in phonetic hiragana or katakana characters."]),
+        "yomiName": StringAttribute, // Pronunciation of the name of the owner, written in phonetic hiragana or katakana characters.
       ])
       .registerPath("applications_owners");
   }
--- source/models/applications/entities/territory.d
+++ source/models/applications/entities/territory.d
@@ -12,17 +12,17 @@ class DTerritoryEntity : DOOPEntity {
 
     this
       .addValues([
-        "createdOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who created the territory."]),
-        "modifiedOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who last modified the territory."]),
-        "organizationId": UUIDAttribute, // Unique identifier for the organization	applicationCommon"]),
-        "importSequenceNumber": NumberAttribute, // Sequence number of the import that created this record."]),
-        "overriddenCreatedOn": TimestampAttribute, // Date and time that the record was migrated."]),
-        "timeZoneRuleVersionNumber": NumberAttribute, // For internal use only."]),
-        "utcConversionTimeZoneCode": IntegerAttribute, // Time zone code that was in use when the record was created."]),
-        "managerId": UUIDAttribute, // Unique identifier of the manager of the territory."]),
-        "entityImageId": UUIDAttribute, // applicationCommon"]),
-        "exchangeRate": StringAttribute, // Exchange rate for the currency associated with the territory with respect to the base currency."]),
-        "transactionCurrencyId": CurrencyIdAttribute, // Unique identifier of the currency associated with the territory."]),
+        "createdOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who created the territory.
+        "modifiedOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who last modified the territory.
+        "organizationId": UUIDAttribute, // Unique identifier for the organization	applicationCommon
+        "importSequenceNumber": NumberAttribute, // Sequence number of the import that created this record.
+        "overriddenCreatedOn": TimestampAttribute, // Date and time that the record was migrated.
+        "timeZoneRuleVersionNumber": NumberAttribute, // For internal use only.
+        "utcConversionTimeZoneCode": IntegerAttribute, // Time zone code that was in use when the record was created.
+        "managerId": UUIDAttribute, // Unique identifier of the manager of the territory.
+        "entityImageId": UUIDAttribute, // applicationCommon
+        "exchangeRate": StringAttribute, // Exchange rate for the currency associated with the territory with respect to the base currency.
+        "transactionCurrencyId": CurrencyIdAttribute, // Unique identifier of the currency associated with the territory.
       ])
       .registerPath("applications_territories");
   }
