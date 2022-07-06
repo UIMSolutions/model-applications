83bfef6a95d48d23ae5c8f82bb6884c7f445850d
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-07-06 06:28:08 +0200

    Update comments

--- source/models/applications/entities/position.d
+++ source/models/applications/entities/position.d
@@ -12,20 +12,20 @@ class DPositionEntity : DOOPEntity {
 
     this
       .addValues([
-        "createdOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who created the record."]),
-        "modifiedOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who modified the record."]),
-        "organizationId": UUIDAttribute, // Unique identifier for the organization"]),
-        "stateCode": IntegerAttribute, // Status of the Position"]),
+        "createdOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who created the record.
+        "modifiedOnBehalfBy": UserIdAttribute, // Unique identifier of the delegate user who modified the record.
+        "organizationId": UUIDAttribute, // Unique identifier for the organization
+        "stateCode": IntegerAttribute, // Status of the Position
         "stateCode_display": StringAttribute, // 
-        "statusCode": IntegerAttribute, // Reason for the status of the Position"]),
+        "statusCode": IntegerAttribute, // Reason for the status of the Position
         "statusCode_display": StringAttribute, // 
-        "importSequenceNumber": NumberAttribute, // Sequence number of the import that created this record."]),
-        "overriddenCreatedOn": TimestampAttribute, // Date and time that the record was migrated."]),
-        "timeZoneRuleVersionNumber": NumberAttribute, // For internal use only."]),
-        "utcConversionTimeZoneCode": IntegerAttribute, // Time zone code that was in use when the record was created."]),
-        "parentPositionId": UUIDAttribute, // Parent position."]),
-        "exchangeRate": StringAttribute, // Exchange rate for the currency associated with the position with respect to the base currency."]),
-        "transactionCurrencyId": CurrencyIdAttribute, // Unique identifier of the currency associated with the position."]),
+        "importSequenceNumber": NumberAttribute, // Sequence number of the import that created this record.
+        "overriddenCreatedOn": TimestampAttribute, // Date and time that the record was migrated.
+        "timeZoneRuleVersionNumber": NumberAttribute, // For internal use only.
+        "utcConversionTimeZoneCode": IntegerAttribute, // Time zone code that was in use when the record was created.
+        "parentPositionId": UUIDAttribute, // Parent position.
+        "exchangeRate": StringAttribute, // Exchange rate for the currency associated with the position with respect to the base currency.
+        "transactionCurrencyId": CurrencyIdAttribute, // Unique identifier of the currency associated with the position.
       ])
       .registerPath("applications_positions");
   }
